6c370c116faa40726ebf7ec17e5d5905f85df82f
sergiohernandeznavarro89@gmail.com <sergiohernandeznavarro89@gmail.com>
2023-09-06 09:26:01 +0200

    get all items

---
+++ Application/QueryHandlers/GetAllItemQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Application.QueryHandlers;
+
+public class GetAllItemQueryHandler : IRequestHandler<GetItemQuery, List<ItemResponse>>
+{
+
+    private readonly IMapper _mapper;
+    private readonly IItemSummaryQueryRepository _itemSummaryQueryRepository;
+
+    public GetAllItemQueryHandler(IMapper mapper, IItemSummaryQueryRepository itemSummaryQueryRepository)
+    {
+        _mapper = mapper;
+        _itemSummaryQueryRepository = itemSummaryQueryRepository;        
+    }
+
+    public async Task<List<ItemResponse>> Handle(GetItemQuery request, CancellationToken cancellationToken)
+    {
+        var response = new List<ItemResponse>();        
+
+        var items = await _itemSummaryQueryRepository.FindByUserId(request.UserId);        
+
+        response = _mapper.Map<List<ItemResponse>>(items);
+
+        return response;
+    }    
+}
