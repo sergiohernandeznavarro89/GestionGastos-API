5264b0b6cdce975610f56dbf67793acea5d67e8c
sergiohernandeznavarro89@gmail.com <sergiohernandeznavarro89@gmail.com>
2023-09-09 13:54:15 +0200

    resolve bug in AddItem

--- Application/Mappers/ItemMapper.cs
+++ Application/Mappers/ItemMapper.cs
@@ -3,8 +3,10 @@
 public class ItemMapper : Profile
 {
     public ItemMapper() 
-    {        
-        CreateMap<AddItemCommand, Item>();
+    {
+        CreateMap<AddItemCommand, Item>()
+            .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => DateTime.Parse(src.StartDate)))
+            .ForMember(dest => dest.EndDate, opt => opt.MapFrom(src => DateTime.Parse(src.EndDate)));
         CreateMap<ItemSummary, PendingPayItemResponse>();
         CreateMap<ItemSummary, ItemResponse>();
         CreateMap<ItemSummary, NextMonthPendingPayItemResponse>();
